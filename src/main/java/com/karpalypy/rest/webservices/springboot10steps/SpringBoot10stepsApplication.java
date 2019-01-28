f86794db8a921d16cd6c0411d88a14530e4b9754
Karly Toloza <ktoloza@ogangi.com>
2019-01-28 11:43:55 -0300

    init

---
+++ src/main/java/com/karpalypy/rest/webservices/springboot10steps/SpringBoot10stepsApplication.java
@@ -0,0 +1,14 @@
+package com.karpalypy.rest.webservices.springboot10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot10stepsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/karpalypy/rest/webservices/springboot10steps/SpringBoot10stepsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.karpalypy.rest.webservices.springboot10steps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot10stepsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
